845b3f093c095f6c5315b6d202a64c226cdf1722
Lakshay <aroralakshay67@gmail.com>
2025-08-26 16:45:20 +0530

    Initial commit: E-commerce project with admin, backend, frontend

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src="" alt="" className="nav-logo" />
+
+    </div>
+  )
+}
+
+export default Navbar
